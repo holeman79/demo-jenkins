9342cf8aa3e2171bb7d3c713d2b89821456ebd4e
조원진 <>
2022-02-27 11:40:09 +0900

    getProducts 메소드 추가

--- src/main/java/com/example/demojenkins/api/ProductApi.java
+++ src/main/java/com/example/demojenkins/api/ProductApi.java
@@ -1,10 +1,14 @@
 package com.example.demojenkins.api;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductApi {
+    @GetMapping
+    public void getProducts() {
 
+    }
 }
