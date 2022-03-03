16fc774963ee2658622c7238c22b60079ca1610d
조원진 <>
2022-03-03 18:24:34 +0900

    Product Api 수정

--- src/main/java/com/example/demojenkins/api/ProductApi.java
+++ src/main/java/com/example/demojenkins/api/ProductApi.java
@@ -1,14 +1,16 @@
 package com.example.demojenkins.api;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/products")
+@Slf4j
 public class ProductApi {
     @GetMapping
-    public void getProducts() {
-
+    public String getProducts() {
+        return "In Product Api";
     }
 }
