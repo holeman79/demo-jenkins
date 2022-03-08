dcdc0924adea6d19619a7703e8b9840f03c5d762
조원진 <>
2022-03-08 16:44:27 +0900

    timestamp 확인

--- src/main/java/com/example/demojenkins/DemoJenkinsApplication.java
+++ src/main/java/com/example/demojenkins/DemoJenkinsApplication.java
@@ -3,10 +3,20 @@ package com.example.demojenkins;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 @SpringBootApplication
 public class DemoJenkinsApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("LocalDateTime.now : " + LocalDateTime.now());
+		System.out.println("ZonedDateTimeToLocalDateTime : " + ZonedDateTime.now(ZoneId.of("Asia/Seoul")).toLocalDateTime());
+		System.out.println("LocalDateTime.now(Asia/Seoul)" + LocalDateTime.now(ZoneId.of("Asia/Seoul")));
+		System.out.println("ZonedDateTime.now : " + ZonedDateTime.now());
+		System.out.println("ZonedDateTime.now(Asia/Seoul) : " + ZonedDateTime.now(ZoneId.of("Asia/Seoul")));
 		SpringApplication.run(DemoJenkinsApplication.class, args);
 	}
 
